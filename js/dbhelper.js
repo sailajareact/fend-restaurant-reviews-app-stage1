65a9272a5c96e0cf3606d6a49efd382f3c1abe06
Sailaja Nandela <snandyala0@gmail.com>
2018-09-25 02:38:23 -0500

    add media queries to retaurant list page

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,7 +8,7 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8006 // Change this to your server port
+    const port = 8007 // Change this to your server port
     return `http://localhost:${port}/data/restaurants.json`;
   }
 
